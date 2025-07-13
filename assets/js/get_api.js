af206e91251248fd13eee7bb27d8e80c3ad343f7
madarom <madarom.compte10@gmail.com>
2025-07-13 21:57:37 +0300

    Update get_api.js

--- assets/js/get_api.js
+++ assets/js/get_api.js
@@ -116,10 +116,10 @@ function showDetail(productId) {
           </div>
         </div>
         <div class="w-full lg:w-1/2">
-          <h2 class="text-4xl font-bold mb-4">${product.name_fr}</h2>
+        <p class="text-4xl font-bold mb-4">Nom latin : ${product.name_latin ?? '–'}</p>
+          <h2 class="text-2xl font-bold mb-4">${product.name_fr}</h2>
           <p class="text-gray-700 mb-4">${product.description_fr}</p>
           <p class="text-gray-600 mb-4">Catégorie : ${product.category?.name ?? '–'}</p>
-          <p class="text-gray-600 mb-4">Nom latin : ${product.name_latin ?? '–'}</p>
           <button class="mt-6 btn-primary text-white px-6 py-2 rounded-full" onclick="hideDetail()">Fermer</button>
         </div>
       </div>
